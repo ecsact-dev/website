2dcfeb97abd03a390ea663c899d2420b04fefe62
Ezekiel Warren <zekewarren@gmail.com>
2022-07-19 01:19:43 -0700

    more language docs, table style

--- src/app/docs/lang/lang.component.ts
+++ src/app/docs/lang/lang.component.ts
@@ -1,11 +1,21 @@
 import {Component, OnInit, ChangeDetectionStrategy} from '@angular/core';
 
+const DEFINE_COMPONENT_SAMPLE_01 = `
+package example;
+
+component Position {
+	int32 x;
+	int32 y;
+}
+`.trim();
+
 @Component({
 	templateUrl: './lang.component.html',
 	styleUrls: ['./lang.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LangComponent implements OnInit {
+	readonly DEFINE_COMPONENT_SAMPLE_01 = DEFINE_COMPONENT_SAMPLE_01;
 	constructor() {}
 
 	ngOnInit(): void {}
