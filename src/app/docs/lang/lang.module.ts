206a2c2e1a36f19773cd0bdee743768669800faa
Ezekiel Warren <zekewarren@gmail.com>
2022-07-20 16:56:18 -0700

    Syntax highlighting

--- src/app/docs/lang/lang.module.ts
+++ src/app/docs/lang/lang.module.ts
@@ -1,10 +1,11 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {LangComponent} from './lang.component';
+import {EcsactLangSyntaxModule} from '../../../components/ecsact-lang-syntax/ecsact-lang-syntax.module';
 
 @NgModule({
 	declarations: [LangComponent],
-	imports: [CommonModule],
+	imports: [CommonModule, EcsactLangSyntaxModule],
 	exports: [LangComponent],
 })
 export class LangModule {}
---
+++ src/components/ecsact-lang-syntax/ecsact-lang-syntax.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EcsactLangSyntaxComponent } from './ecsact-lang-syntax.component';
+
+
+
+@NgModule({
+  declarations: [
+    EcsactLangSyntaxComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    EcsactLangSyntaxComponent
+  ]
+})
+export class EcsactLangSyntaxModule { }
