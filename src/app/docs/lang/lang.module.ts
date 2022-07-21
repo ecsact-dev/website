59bdc2f900420ec501840890d8f3c1aef01f29d8
Ezekiel Warren <zekewarren@gmail.com>
2022-07-20 20:07:52 -0700

    More documentation and page anchor tweak

--- src/app/docs/lang/lang.module.ts
+++ src/app/docs/lang/lang.module.ts
@@ -2,10 +2,11 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {LangComponent} from './lang.component';
 import {EcsactLangSyntaxModule} from '../../../components/ecsact-lang-syntax/ecsact-lang-syntax.module';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
 	declarations: [LangComponent],
-	imports: [CommonModule, EcsactLangSyntaxModule],
+	imports: [CommonModule, EcsactLangSyntaxModule, RouterModule],
 	exports: [LangComponent],
 })
 export class LangModule {}
