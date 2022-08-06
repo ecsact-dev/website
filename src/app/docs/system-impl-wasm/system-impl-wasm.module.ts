7a562daaa2fa8a9e5cebb4e14f16f36a5da08151
Ezekiel Warren <zekewarren@gmail.com>
2022-08-05 23:06:08 -0700

    Added a little validator

--- src/app/docs/system-impl-wasm/system-impl-wasm.module.ts
+++ src/app/docs/system-impl-wasm/system-impl-wasm.module.ts
@@ -1,10 +1,11 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {SystemImplWasmComponent} from './system-impl-wasm.component';
+import {EcsactWasmSystemImplValidatorModule} from '../../../components/ecsact-wasm-system-impl-validator/ecsact-wasm-system-impl-validator.module';
 
 @NgModule({
 	declarations: [SystemImplWasmComponent],
-	imports: [CommonModule],
+	imports: [CommonModule, EcsactWasmSystemImplValidatorModule],
 	exports: [SystemImplWasmComponent],
 })
 export class SystemImplWasmModule {}
---
+++ src/components/ecsact-wasm-system-impl-validator/ecsact-wasm-system-impl-validator.component.ts
@@ -0,0 +1,141 @@
+import {
+	Component,
+	OnInit,
+	ChangeDetectionStrategy,
+	ViewChild,
+	ElementRef,
+	ChangeDetectorRef,
+} from '@angular/core';
+
+export enum EcsactWasmImportValidationCode {
+	Ok = 'Ok',
+	NonFunction = 'NonFunction',
+	UnknownImportName = 'UnknownImportName',
+}
+
+export interface EcsactWasmImportPreview {
+	descriptor: WebAssembly.ModuleImportDescriptor;
+	validationCode: EcsactWasmImportValidationCode;
+}
+
+export enum EcsactWasmExportValidationCode {
+	Ok = 'Ok',
+	NonFunction = 'NonFunction',
+	InvalidSignature = 'InvalidSignature',
+}
+
+export interface EcsactWasmExportPreview {
+	descriptor: WebAssembly.ModuleExportDescriptor;
+	validationCode: EcsactWasmExportValidationCode;
+}
+
+export interface EcsactWasmModulePreviewItem {
+	name: string;
+	imports: EcsactWasmImportPreview[];
+	exports: EcsactWasmExportPreview[];
+}
+
+function toExportPreview(descriptor: WebAssembly.ModuleExportDescriptor) {
+	let validationCode = EcsactWasmExportValidationCode.Ok;
+	if (descriptor.kind !== 'function') {
+		validationCode = EcsactWasmExportValidationCode.NonFunction;
+	}
+	return {descriptor, validationCode};
+}
+
+function toImportPreview(descriptor: WebAssembly.ModuleImportDescriptor) {
+	const prefix = 'ecsact_system_execution_context_';
+	let validationCode = EcsactWasmImportValidationCode.Ok;
+
+	if (descriptor.name.indexOf(prefix) != 0) {
+		validationCode = EcsactWasmImportValidationCode.UnknownImportName;
+	} else if (descriptor.kind !== 'function') {
+		validationCode = EcsactWasmImportValidationCode.NonFunction;
+	}
+
+	return {descriptor, validationCode};
+}
+
+@Component({
+	selector: 'ecsact-wasm-system-impl-validator',
+	templateUrl: './ecsact-wasm-system-impl-validator.component.html',
+	styleUrls: ['./ecsact-wasm-system-impl-validator.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class EcsactWasmSystemImplValidatorComponent implements OnInit {
+	@ViewChild('fileInput', {static: true})
+	fileInput?: ElementRef<HTMLInputElement>;
+
+	previewModules: EcsactWasmModulePreviewItem[] = [];
+
+	constructor(private cdr: ChangeDetectorRef) {}
+
+	ngOnInit(): void {}
+
+	onFileInput(ev: any) {
+		this.previewModules = [];
+
+		const files = this.fileInput!.nativeElement.files;
+		const promises: Promise<void>[] = [];
+
+		for (const file of Array.from(files)) {
+			this.previewModules.push({
+				name: file.name,
+				exports: [],
+				imports: [],
+			});
+
+			const item = this.previewModules[this.previewModules.length - 1];
+
+			promises.push(
+				file.arrayBuffer().then(buffer => {
+					return WebAssembly.compile(buffer).then(wasmModule => {
+						item.imports =
+							WebAssembly.Module.imports(wasmModule).map(toImportPreview);
+						item.exports =
+							WebAssembly.Module.exports(wasmModule).map(toExportPreview);
+
+						const fauxImportFns: any = {};
+
+						for (const imp of item.imports) {
+							if (imp.descriptor.kind === 'function') {
+								fauxImportFns[imp.descriptor.name] = function () {};
+							}
+						}
+
+						WebAssembly.instantiate(wasmModule, {
+							env: {
+								...fauxImportFns,
+							},
+						}).then(inst => {
+							for (const wasmExportName in inst.exports) {
+								for (const preview of item.exports) {
+									if (preview.descriptor.name === wasmExportName) {
+										const wasmExport = inst.exports[wasmExportName];
+										if (typeof wasmExport === 'function') {
+											if (wasmExport.length !== 1) {
+												preview.validationCode =
+													EcsactWasmExportValidationCode.InvalidSignature;
+												this.cdr.detectChanges();
+											}
+										}
+										break;
+									}
+								}
+							}
+						});
+					});
+				}),
+			);
+
+			Promise.all(promises)
+				.then(() => {
+					this.cdr.detectChanges();
+				})
+				.catch(err => {
+					console.error(err);
+					this.cdr.detectChanges();
+				});
+		}
+	}
+}
