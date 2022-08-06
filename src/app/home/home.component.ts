2455da212caf3dc09bc5c2b8c92decbb1dd26948
Ezekiel Warren <zekewarren@gmail.com>
2022-08-06 16:31:43 -0700

    Home page revamp

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,15 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 
+import {detectOS} from 'detect-browser';
+
 @Component({
 	templateUrl: 'home.component.html',
 	styleUrls: ['home.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent {}
+export class HomeComponent {
+	os = detectOS(navigator.userAgent);
+	latestRelease = fetch(
+		'https://api.github.com/repos/seaube/ecsact-sdk/releases/latest',
+	).then(res => res.json());
+}
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,11 +1,13 @@
+import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
+import {EcsactLangSyntaxModule} from '../../components/ecsact-lang-syntax/ecsact-lang-syntax.module';
 
 import {HomeComponent} from './home.component';
 
 @NgModule({
 	declarations: [HomeComponent],
-	imports: [RouterModule],
+	imports: [RouterModule, CommonModule, EcsactLangSyntaxModule],
 	exports: [HomeComponent],
 	bootstrap: [],
 })
