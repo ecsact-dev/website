5f1fac9bff1ed3bc9efbe3203b1eb705e79909dd
Ezekiel Warren <zekewarren@gmail.com>
2022-08-05 18:53:58 -0700

    Added example ecsact to system impl capabilities pag

--- src/app/docs/system-impl/system-impl.module.ts
+++ src/app/docs/system-impl/system-impl.module.ts
@@ -2,10 +2,11 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {SystemImplComponent} from './system-impl.component';
 import {HiddenTableRowsModule} from '../../../components/hidden-table-rows/hidden-table-rows.module';
+import {EcsactLangSyntaxModule} from '../../../components/ecsact-lang-syntax/ecsact-lang-syntax.module';
 
 @NgModule({
 	declarations: [SystemImplComponent],
-	imports: [CommonModule, HiddenTableRowsModule],
+	imports: [CommonModule, HiddenTableRowsModule, EcsactLangSyntaxModule],
 	exports: [SystemImplComponent],
 })
 export class SystemImplModule {}
