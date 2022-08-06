e432f1acc10d0a840ee16fe7421ff0ece7004b00
Ezekiel Warren <zekewarren@gmail.com>
2022-08-05 23:06:08 -0700

    Added a little validator

---
+++ src/components/ecsact-wasm-system-impl-validator/ecsact-wasm-system-impl-validator.module.ts
@@ -0,0 +1,10 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {EcsactWasmSystemImplValidatorComponent} from './ecsact-wasm-system-impl-validator.component';
+
+@NgModule({
+	declarations: [EcsactWasmSystemImplValidatorComponent],
+	imports: [CommonModule],
+	exports: [EcsactWasmSystemImplValidatorComponent],
+})
+export class EcsactWasmSystemImplValidatorModule {}
