e2046dd9c719e84b186e081774a5e3283b4e3f14
Ezekiel Warren <zekewarren@gmail.com>
2022-08-07 01:24:33 -0700

    ecsact diagram

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,10 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {
+	ChangeDetectionStrategy,
+	ChangeDetectorRef,
+	Component,
+	OnDestroy,
+	OnInit,
+} from '@angular/core';
 
 import {detectOS} from 'detect-browser';
 
@@ -7,9 +13,31 @@ import {detectOS} from 'detect-browser';
 	styleUrls: ['home.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
 	os = detectOS(navigator.userAgent);
 	latestRelease = fetch(
 		'https://api.github.com/repos/seaube/ecsact-sdk/releases/latest',
 	).then(res => res.json());
+
+	private mediaQuery: MediaQueryList;
+	private readonly onMediaQueryChange: (ev: MediaQueryListEvent) => void;
+
+	constructor(cdr: ChangeDetectorRef) {
+		this.mediaQuery = window.matchMedia('(max-width: 1024px)');
+		this.onMediaQueryChange = ev => {
+			cdr.detectChanges();
+		};
+	}
+
+	public smallWidth() {
+		return this.mediaQuery.matches;
+	}
+
+	ngOnInit() {
+		this.mediaQuery.addEventListener('change', this.onMediaQueryChange);
+	}
+
+	ngOnDestroy() {
+		this.mediaQuery.removeEventListener('change', this.onMediaQueryChange);
+	}
 }
---
+++ src/components/ecsact-example-diagram/ecsact-example-diagram.module.ts
@@ -0,0 +1,10 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {EcsactExampleDiagramComponent} from './ecsact-example-diagram.component';
+
+@NgModule({
+	declarations: [EcsactExampleDiagramComponent],
+	imports: [CommonModule],
+	exports: [EcsactExampleDiagramComponent],
+})
+export class EcsactExampleDiagramModule {}
