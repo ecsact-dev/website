03a4a844562cc0b256793976bb55ae6ca8c8d89c
Ezekiel Warren <zekewarren@gmail.com>
2022-08-08 12:27:58 -0700

    First draft of emsdk tutorial complete

--- src/app/tutorials/sys-impl/emsdk-cpp/emsdk-cpp.module.ts
+++ src/app/tutorials/sys-impl/emsdk-cpp/emsdk-cpp.module.ts
@@ -1,18 +1,18 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { EmsdkCppComponent } from './emsdk-cpp.component';
-
-
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {EmsdkCppComponent} from './emsdk-cpp.component';
+import {RouterModule} from '@angular/router';
+import {EcsactLangSyntaxModule} from '../../../../components/ecsact-lang-syntax/ecsact-lang-syntax.module';
+import {EcsactWasmSystemImplValidatorModule} from '../../../../components/ecsact-wasm-system-impl-validator/ecsact-wasm-system-impl-validator.module';
 
 @NgModule({
-  declarations: [
-    EmsdkCppComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    EmsdkCppComponent
-  ]
+	declarations: [EmsdkCppComponent],
+	imports: [
+		CommonModule,
+		RouterModule,
+		EcsactLangSyntaxModule,
+		EcsactWasmSystemImplValidatorModule,
+	],
+	exports: [EmsdkCppComponent],
 })
-export class EmsdkCppModule { }
+export class EmsdkCppModule {}
