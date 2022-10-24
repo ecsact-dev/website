09f01f717f7b8068403bb35e6d70fb5be929a96a
Ezekiel Warren <zekewarren@gmail.com>
2022-10-24 13:29:59 -0700

    doxygen location links to github now

--- src/components/doxygen-member-components/doxygen-location/doxygen-location.component.ts
+++ src/components/doxygen-member-components/doxygen-location/doxygen-location.component.ts
@@ -1,4 +1,10 @@
-import {Component, OnInit, ChangeDetectionStrategy, Input} from '@angular/core';
+import {
+	Component,
+	ChangeDetectionStrategy,
+	Input,
+	ChangeDetectorRef,
+} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 import {DoxygenDefLocation} from '../../../search/doxygen-def-types';
 
 @Component({
@@ -11,5 +17,36 @@ export class DoxygenLocationComponent {
 	@Input()
 	location: DoxygenDefLocation;
 
-	constructor() {}
+	private _repo: string = '';
+	@Input()
+	set repo(value: string) {
+		this._repo = value;
+	}
+	get repo() {
+		return this._repo || this.route.snapshot.params.repo;
+	}
+
+	get href(): string {
+		// TODO(zaucy): Use commit from `repo.json`
+		const commit = `main`;
+		let href = `https://github.com/ecsact-dev/${this.repo}/blob/${commit}/`;
+		href += this.location.file;
+		if (!isNaN(this.location.line)) {
+			href += `#L${this.location.line}`;
+		}
+		return href;
+	}
+
+	get displayFilePath() {
+		let result = this.location.file;
+		if (!isNaN(this.location.line)) {
+			result += `:${this.location.line}:${this.location.column}`;
+		}
+
+		return result;
+	}
+
+	constructor(private route: ActivatedRoute, cdr: ChangeDetectorRef) {
+		route.params.subscribe(() => cdr.markForCheck());
+	}
 }
