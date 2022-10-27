32cc2c9df62e71d21e7fba2df045e642466bb173
Ezekiel Warren <zekewarren@gmail.com>
2022-10-26 21:42:57 -0700

    button style and download links

--- src/app/getstarted/getstarted.component.ts
+++ src/app/getstarted/getstarted.component.ts
@@ -1,8 +1,11 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {detectOS, OperatingSystem} from 'detect-browser';
 
 @Component({
 	templateUrl: 'getstarted.component.html',
 	styleUrls: ['getstarted.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class GetStartedComponent {}
+export class GetStartedComponent {
+	readonly os: OperatingSystem = detectOS(navigator.userAgent);
+}
