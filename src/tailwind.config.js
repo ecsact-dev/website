77eaa4322f6b987d8f440daeda6230efb0102432
Ezekiel Warren <zekewarren@gmail.com>
2022-10-28 18:00:42 -0700

    home page little more spiffy + tailwind refactors

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -4,7 +4,15 @@ const content = process.argv.slice(process.argv.indexOf('--') + 1);
 module.exports = {
 	content,
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: {
+				logoWhite: 'url(external/ecsact_logo/ecsact-white.png)',
+				logoBlack: 'url(external/ecsact_logo/ecsact-black.png)',
+				logoColor: 'url(external/ecsact_logo/ecsact-color.png)',
+				'subtle-texture': 'url(assets/bg/subtle-texture.webp)',
+				'connect-pattern': 'url(assets/bg/connect-pattern.png)',
+			},
+		},
 		fontFamily: {
 			sans: ['Raleway', 'ui-sans-serif', 'system-ui'],
 			serif: ['Raleway', 'ui-serif', 'sans-serif'],
@@ -15,6 +23,8 @@ module.exports = {
 			secondary: '#ff8811',
 			error: 'rgb(221 100 70)',
 			success: 'rgb(86 213 108)',
+			white: '#d4d4d4',
+			black: '#202120',
 		},
 	},
 	plugins: [],
