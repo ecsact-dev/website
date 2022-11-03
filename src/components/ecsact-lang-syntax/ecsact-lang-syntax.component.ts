92b0191b0828f218d66556c69f6e05a726360ae7
Ezekiel Warren <zekewarren@gmail.com>
2022-11-03 10:39:19 -0700

    Fixed ecsact syntax highlighting

--- src/components/ecsact-lang-syntax/ecsact-lang-syntax.component.ts
+++ src/components/ecsact-lang-syntax/ecsact-lang-syntax.component.ts
@@ -16,32 +16,41 @@ languages.ecsact = {
 		greedy: true,
 	},
 	keyword: {
-		pattern: /component|system|action|package|main|import/,
+		pattern: /package|main|import/,
 	},
 	function: {
 		pattern:
-			/required|readonly|readwrite|readonly|optional|adds|removes|include|exclude/,
+			/required|readonly|readwrite|readonly|optional|adds|removes|include|exclude|with/,
 	},
 	variable: {
-		pattern:
-			/(int8|uint8|int16|uint16|int32|uint32|bool|decimal32|entity)\s+[A-Za-z_]+/,
+		pattern: /(i8|u8|i16|u16|i32|u32|bool|f32|entity)\s+[A-Za-z_]+/,
 		lookbehind: true,
 	},
 	'ecsact-type-name': {
-		pattern: /((component|system|action)\s+)[A-Za-z]+/,
+		pattern: /((component|system|action|enum)\s+)[A-Za-z0-9]+/,
+		inside: {
+			keyword: {
+				pattern: /component|system|action|enum/,
+			},
+			'class-name': {
+				pattern: /[A-Za-z0-9]+/,
+			},
+		},
+	},
+	'ecsact-with-field-name': {
+		pattern: /(((with)\s+)+)[A-Za-z_]+/,
 		lookbehind: true,
 		greedy: true,
-		alias: ['class-name'],
+		alias: ['variable'],
 	},
 	'ecsact-system-component-name': {
-		pattern:
-			/(((required\s+readonly|optional\s+readonly|required|optional|readonly|exclude|include|adds|removes)\s+)+)[A-Za-z]+/,
+		pattern: /((readwrite|readonly|exclude|include|adds|removes)\s+)[A-Za-z]+/,
 		lookbehind: true,
 		greedy: true,
 		alias: ['class-name'],
 	},
 	builtin: {
-		pattern: /int8|uint8|int16|uint16|int32|uint32|bool|decimal32|entity/,
+		pattern: /i8|u8|i16|u16|i32|u32|bool|f32|entity/,
 	},
 	italic: {
 		pattern: /statement/,
