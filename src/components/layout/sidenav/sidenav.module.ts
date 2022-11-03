d310863e49469efa888ac34cb207239c8ffec816
Ezekiel Warren <zekewarren@gmail.com>
2022-11-03 15:17:41 -0700

    Expandable sidenav

--- src/components/layout/sidenav/sidenav.module.ts
+++ src/components/layout/sidenav/sidenav.module.ts
@@ -1,10 +1,11 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {SidenavComponent} from './sidenav.component';
+import {CdkAccordionModule} from '@angular/cdk/accordion';
 
 @NgModule({
 	declarations: [SidenavComponent],
-	imports: [CommonModule],
+	imports: [CommonModule, CdkAccordionModule],
 	exports: [SidenavComponent],
 })
 export class SidenavModule {}
