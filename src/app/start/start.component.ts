94da9b79dffa0516845638fc4896f2e6f89903e2
Ezekiel Warren <zekewarren@gmail.com>
2022-11-03 23:59:55 -0700

    Renamed getstarted -> start

--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -2,10 +2,10 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {detectOS, OperatingSystem} from 'detect-browser';
 
 @Component({
-	templateUrl: 'getstarted.component.html',
-	styleUrls: ['getstarted.component.scss'],
+	templateUrl: 'start.component.html',
+	styleUrls: ['start.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class GetStartedComponent {
+export class StartComponent {
 	readonly os: OperatingSystem = detectOS(navigator.userAgent);
 }
