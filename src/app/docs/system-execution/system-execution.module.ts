3e3fc0e81f7dc626d58db66a3e4023960bdb9f77
Ezekiel Warren <zekewarren@gmail.com>
2022-11-07 14:15:06 -0800

    Detailed system execution

--- src/app/docs/system-execution/system-execution.module.ts
+++ src/app/docs/system-execution/system-execution.module.ts
@@ -3,10 +3,16 @@ import {CommonModule} from '@angular/common';
 import {SystemExecutionComponent} from './system-execution.component';
 import {EcsactLangSyntaxModule} from '../../../components/ecsact-lang-syntax/ecsact-lang-syntax.module';
 import {ExecLaneVisModule} from '../../../components/exec-lane-vis/exec-lane-vis.module';
+import {CodeBlockVariationModule} from '../../../components/code-block-variation/code-block-variation.module';
 
 @NgModule({
 	declarations: [SystemExecutionComponent],
-	imports: [CommonModule, EcsactLangSyntaxModule, ExecLaneVisModule],
+	imports: [
+		CommonModule,
+		EcsactLangSyntaxModule,
+		ExecLaneVisModule,
+		CodeBlockVariationModule,
+	],
 	exports: [SystemExecutionComponent],
 })
 export class SystemExecutionModule {}
