8076b82cbed8f48af02651f83d84f7e1399a99b0
Austin Kelway <austin@kelway.ca>
2022-11-10 16:50:24 -0500

    Initial commit, work with @zaucy refining docs (#50)

--- src/app/docs/unity/system-impl/system-impl.module.ts
+++ src/app/docs/unity/system-impl/system-impl.module.ts
@@ -1,12 +1,13 @@
 import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
 import {CodeBlockVariationModule} from '../../../../components/code-block-variation/code-block-variation.module';
+import {PrismModule} from '../../../../components/prism/prism.module';
 
 import {SystemImplComponent} from './system-impl.component';
 
 @NgModule({
 	declarations: [SystemImplComponent],
-	imports: [RouterModule, CodeBlockVariationModule],
+	imports: [RouterModule, CodeBlockVariationModule, PrismModule],
 	exports: [SystemImplComponent],
 	bootstrap: [],
 })
