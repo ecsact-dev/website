3f672eeb9b2dbd3c89c646760fc0d4f510ee9ed4
Ezekiel Warren <zekewarren@gmail.com>
2022-11-11 13:14:57 -0800

    Stubbing out roadmap display

--- src/app/roadmap/roadmap.component.ts
+++ src/app/roadmap/roadmap.component.ts
@@ -1,4 +1,11 @@
 import {Component, ChangeDetectionStrategy} from '@angular/core';
+import {BehaviorSubject, from, Observable} from 'rxjs';
+
+export interface IRoadmapProject {
+	title: string;
+	shortDescription: string;
+	readme: string;
+}
 
 @Component({
 	selector: 'ecsact-roadmap',
@@ -6,4 +13,17 @@ import {Component, ChangeDetectionStrategy} from '@angular/core';
 	styleUrls: ['./roadmap.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RoadmapComponent {}
+export class RoadmapComponent {
+	readonly projects$: Observable<IRoadmapProject[]>;
+
+	constructor() {
+		// TODO(zaucy): Fetch from GitHub
+		this.projects$ = new BehaviorSubject([
+			{
+				title: 'Async API',
+				shortDescription: 'TODO: Fetch description from GitHub',
+				readme: '',
+			},
+		]);
+	}
+}
