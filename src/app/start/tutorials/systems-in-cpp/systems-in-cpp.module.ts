e2332a7bbe668ccb565df8031893d27c99f1bdec
Ezekiel Warren <zekewarren@gmail.com>
2022-11-29 00:19:58 -0800

    systems in cpp

--- src/app/start/tutorials/systems-in-cpp/systems-in-cpp.module.ts
+++ src/app/start/tutorials/systems-in-cpp/systems-in-cpp.module.ts
@@ -1,10 +1,13 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {SystemsInCppComponent} from './systems-in-cpp.component';
+import {RouterModule} from '@angular/router';
+import {PrismModule} from '../../../../components/prism/prism.module';
+import {CodeBlockVariationModule} from '../../../../components/code-block-variation/code-block-variation.module';
 
 @NgModule({
 	declarations: [SystemsInCppComponent],
-	imports: [CommonModule],
+	imports: [CommonModule, RouterModule, PrismModule, CodeBlockVariationModule],
 	exports: [SystemsInCppComponent],
 })
 export class SystemsInCppModule {}
