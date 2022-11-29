d249fd2e5f9c01fb00aa0634fbc73caacb4c7431
Ezekiel Warren <zekewarren@gmail.com>
2022-11-28 23:22:38 -0800

    first ecsact file

--- src/app/start/tutorials/first-ecsact-file/first-ecsact-file.module.ts
+++ src/app/start/tutorials/first-ecsact-file/first-ecsact-file.module.ts
@@ -1,10 +1,18 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FirstEcsactFileComponent} from './first-ecsact-file.component';
+import {CodeBlockVariationModule} from '../../../../components/code-block-variation/code-block-variation.module';
+import {EcsactLangSyntaxModule} from '../../../../components/ecsact-lang-syntax/ecsact-lang-syntax.module';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
 	declarations: [FirstEcsactFileComponent],
-	imports: [CommonModule],
+	imports: [
+		CommonModule,
+		RouterModule,
+		CodeBlockVariationModule,
+		EcsactLangSyntaxModule,
+	],
 	exports: [FirstEcsactFileComponent],
 })
 export class FirstEcsactFileModule {}
