ee790f16fa9a1a80bcf700fc57e45fbef5af0127
Ezekiel Warren <zaucy@users.noreply.github.com>
2024-07-05 14:21:20 -0700

    feat: made runtime module page look nicer (#135)

--- src/app/docs/runtime/runtime.component.ts
+++ src/app/docs/runtime/runtime.component.ts
@@ -1,11 +1,23 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {RouterLink} from '@angular/router';
+import {CommonModule, NgTemplateOutlet} from '@angular/common';
+import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
+import {RouterLink, RouterModule} from '@angular/router';
+
+@Component({
+	selector: '[moduleBlock]',
+	templateUrl: 'runtime-module-block.component.html',
+	standalone: true,
+	imports: [RouterModule, CommonModule],
+})
+export class RuntimeModuleBlock {
+	@Input('moduleBlock')
+	moduleName: string = '';
+}
 
 @Component({
 	templateUrl: 'runtime.component.html',
 	styleUrls: ['runtime.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 	standalone: true,
-	imports: [RouterLink],
+	imports: [RouterLink, NgTemplateOutlet, RuntimeModuleBlock],
 })
 export class RuntimeComponent {}
