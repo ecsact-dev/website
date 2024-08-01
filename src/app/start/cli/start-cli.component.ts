68c076fe08a35027ece0efa2b5c986873fa00e91
Ezekiel <ezekiel@seaube.com>
2024-08-01 10:29:37 -0700

    feat: started on multi secion on start cli page

--- src/app/start/cli/start-cli.component.ts
+++ src/app/start/cli/start-cli.component.ts
@@ -1,11 +1,13 @@
 import {Component, OnInit, ChangeDetectionStrategy} from '@angular/core';
 import {TypedAsciiLogoComponent} from './typed-ascii-logo.component';
+import {RouterLink} from '@angular/router';
+import {PrismComponent} from '../../../components/prism/prism.component';
 
 @Component({
 	templateUrl: './start-cli.component.html',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 	standalone: true,
-	imports: [TypedAsciiLogoComponent],
+	imports: [TypedAsciiLogoComponent, RouterLink, PrismComponent],
 })
 export class StartCliComponent implements OnInit {
 	constructor() {}
