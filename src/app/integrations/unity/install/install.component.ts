5624ab369adbb2645008c402c8d0ed2fca61c393
Ezekiel Warren <zaucy@users.noreply.github.com>
2024-08-01 16:54:35 -0700

    fix: remove usage of unused install route (#164)

--- src/app/integrations/unity/install/install.component.ts
+++
@@ -1,9 +0,0 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
-
-@Component({
-	templateUrl: 'install.component.html',
-	styleUrls: ['install.component.scss'],
-	changeDetection: ChangeDetectionStrategy.OnPush,
-	standalone: true,
-})
-export class UnityInstallPageComponent {}
