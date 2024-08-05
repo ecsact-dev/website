203872c2f2f9640c9de81ffc4c64df996e684dec
Ezekiel Warren <zaucy@users.noreply.github.com>
2024-08-05 11:58:06 -0700

    feat: build recipe docs (#169)
    * feat: build recipe docs
    
    * fix: typos

--- src/app/docs/extend/build-recipe.component.ts
+++ src/app/docs/extend/build-recipe.component.ts
@@ -1,7 +1,9 @@
 import {Component} from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
 	templateUrl: './build-recipe.component.html',
 	standalone: true,
+	imports: [RouterLink],
 })
 export class ExtendBuildRecipeComponent {}
