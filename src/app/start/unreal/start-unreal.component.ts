0fd8eaccf8f402efb024a67ae4a0f6d63b0a3376
Ezekiel <ezekiel@seaube.com>
2025-02-07 14:50:27 -0800

    feat: added initial unreal setup guide

--- src/app/start/unreal/start-unreal.component.ts
+++ src/app/start/unreal/start-unreal.component.ts
@@ -1,9 +1,12 @@
 import {Component, OnInit, ChangeDetectionStrategy} from '@angular/core';
+import {RouterLink} from '@angular/router';
+import {PrismComponent} from '../../../components/prism/prism.component';
 
 @Component({
 	templateUrl: './start-unreal.component.html',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 	standalone: true,
+	imports: [RouterLink, PrismComponent],
 })
 export class StartUnrealComponent implements OnInit {
 	constructor() {}
