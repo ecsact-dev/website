50e63096aee15f79108f18242a0e4591224179a3
Ezekiel <ezekiel@seaube.com>
2025-02-08 13:59:18 -0800

    feat: add unreal runtime doc

--- src/app/start/unreal/runtime/unreal-runtime.component.ts
+++ src/app/start/unreal/runtime/unreal-runtime.component.ts
@@ -1,12 +1,19 @@
 import {Component, OnInit, ChangeDetectionStrategy} from '@angular/core';
 import {RouterLink} from '@angular/router';
 import {PrismComponent} from '../../../../components/prism/prism.component';
+import {CodeBlockVariationComponent} from '../../../../components/code-block-variation/code-block-variation.component';
+import {CodeBlockVariationOptionDirective} from '../../../../components/code-block-variation/code-block-variation-option.directive';
 
 @Component({
 	templateUrl: './unreal-runtime.component.html',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 	standalone: true,
-	imports: [RouterLink, PrismComponent],
+	imports: [
+		RouterLink,
+		PrismComponent,
+		CodeBlockVariationComponent,
+		CodeBlockVariationOptionDirective,
+	],
 })
 export class UnrealRuntimeComponent implements OnInit {
 	constructor() {}
