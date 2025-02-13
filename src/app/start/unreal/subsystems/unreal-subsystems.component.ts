7677bbc7cace696e533e5e03a516b893e5ace3a8
Ezekiel <ezekiel@seaube.com>
2025-02-13 15:30:22 -0800

    feat: add unnreal subsystem documentation

--- src/app/start/unreal/subsystems/unreal-subsystems.component.ts
+++ src/app/start/unreal/subsystems/unreal-subsystems.component.ts
@@ -1,12 +1,21 @@
 import {Component, OnInit, ChangeDetectionStrategy} from '@angular/core';
 import {RouterLink} from '@angular/router';
 import {PrismComponent} from '../../../../components/prism/prism.component';
+import {EcsactLangSyntaxComponent} from '../../../../components/ecsact-lang-syntax/ecsact-lang-syntax.component';
+import {CodeBlockVariationComponent} from '../../../../components/code-block-variation/code-block-variation.component';
+import {CodeBlockVariationOptionDirective} from '../../../../components/code-block-variation/code-block-variation-option.directive';
 
 @Component({
 	templateUrl: './unreal-subsystems.component.html',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 	standalone: true,
-	imports: [RouterLink, PrismComponent],
+	imports: [
+		RouterLink,
+		PrismComponent,
+		EcsactLangSyntaxComponent,
+		CodeBlockVariationComponent,
+		CodeBlockVariationOptionDirective,
+	],
 })
 export class UnrealSubsystemsComponent implements OnInit {
 	constructor() {}
